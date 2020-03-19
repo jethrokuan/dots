b8bb381513bc09ed06559557354fc534dafcbcb1
Jethro Kuan <jethrokuan95@gmail.com>
2020-03-19 22:37:48 +0800

    (emacs): enable latexmk and cdlatex

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -130,7 +130,7 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+       (latex +latexmk +cdlatex)    ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
