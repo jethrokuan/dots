fb69d5b686996c8e1913037342c092c300ff4b2b
Jethro Kuan <jethrokuan95@gmail.com>
2020-06-01 14:31:04 +0800

    (emacs): update export snippet

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -485,20 +485,27 @@
   (set-company-backend! 'org-mode '(company-org-roam company-yasnippet company-dabbrev)))
 
 (after! (org org-roam)
-    (defun jethro/org-roam--backlinks-list (file)
-      (when (org-roam--org-roam-file-p file)
-          (mapcar #'car (org-roam-db-query [:select :distinct [from]
-                                           :from links
-                                           :where (= to $s1)
-                                           :and from :not :like $s2] file "%private%"))))
-    (defun jethro/org-export-preprocessor (_backend)
-      (when-let ((links (jethro/org-roam--backlinks-list (buffer-file-name))))
-        (insert "\n** Backlinks\n")
-        (dolist (link links)
-          (insert (format "- [[file:%s][%s]]\n"
-                          (file-relative-name link org-roam-directory)
-                          (org-roam--get-title-or-slug link))))))
-    (add-hook 'org-export-before-processing-hook #'jethro/org-export-preprocessor))
+  (defun jethro/org-roam-export-all ()
+    "Re-exports all Org-roam files to Hugo markdown."
+    (interactive)
+    (dolist (f (org-roam--list-all-files))
+      (with-current-buffer (find-file f)
+        (when (s-contains? "SETUPFILE" (buffer-string))
+          (org-hugo-export-wim-to-md)))))
+  (defun jethro/org-roam--backlinks-list (file)
+    (when (org-roam--org-roam-file-p file)
+      (mapcar #'car (org-roam-db-query [:select :distinct [from]
+                                        :from links
+                                        :where (= to $s1)
+                                        :and from :not :like $s2] file "%private%"))))
+  (defun jethro/org-export-preprocessor (_backend)
+    (when-let ((links (jethro/org-roam--backlinks-list (buffer-file-name))))
+      (insert "\n** Backlinks\n")
+      (dolist (link links)
+        (insert (format "- [[file:%s][%s]]\n"
+                        (file-relative-name link org-roam-directory)
+                        (org-roam--get-title-or-slug link))))))
+  (add-hook 'org-export-before-processing-hook #'jethro/org-export-preprocessor))
 
 (after! (org ox-hugo)
   (defun jethro/conditional-hugo-enable ()
