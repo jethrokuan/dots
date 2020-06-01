0064ea2aab667f115a14ce48292731db46302c53
Jethro Kuan <jethrokuan95@gmail.com>
2020-06-01 23:35:16 +0800

    (emacs): add jethro/open-with

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -690,3 +690,23 @@
   :hook
   ((sgml-mode . emmet-mode)
    (css-mode . emmet-mode)))
+
+
+(defun jethro/open-with (arg)
+  "Open visited file in default external program.
+When in dired mode, open file under the cursor.
+With a prefix ARG always prompt for command to use."
+  (interactive "P")
+  (let* ((current-file-name
+          (if (eq major-mode 'dired-mode)
+              (dired-get-file-for-visit)
+            buffer-file-name))
+         (open (pcase system-type
+                 (`darwin "open")
+                 ((or `gnu `gnu/linux `gnu/kfreebsd) "xdg-open")))
+         (program (if (or arg (not open))
+                      (read-shell-command "Open current file with: ")
+                    open)))
+    (call-process program nil 0 nil current-file-name)))
+
+(map! "C-c o o" 'jethro/open-with)
