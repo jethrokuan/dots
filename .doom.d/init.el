9be7b907d67e2c46ec3f262d2ec256c1e373d147
Jethro Kuan <jethrokuan95@gmail.com>
2020-11-09 13:11:54 +0800

    enable RSS

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -173,7 +173,7 @@
        :app
        ;;calendar
        ;;irc               ; how neckbeards socialize
-       ;; (rss +org)       ; emacs as an RSS reader
+       (rss +org)       ; emacs as an RSS reader
        ;; twitter           ; twitter client https://twitter.com/vnought
 
        :config
